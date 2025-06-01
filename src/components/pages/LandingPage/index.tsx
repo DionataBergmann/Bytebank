002359e75fa1550ea1abd179405849733094bd95
DionataBergmann <dionata.podewils@outlook.com>
2025-06-01 13:49:10 -0300

    :sparkles: Implements login modal

--- src/components/pages/LandingPage/index.tsx
+++ src/components/pages/LandingPage/index.tsx
@@ -5,16 +5,17 @@ import Header from "./Header";
 import HeroSection from "./HeroSection";
 
 export default function LandingPage() {
+  const [isOpenLoginModal, setIsOpenLoginModal] = useState(false)
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header setIsOpenLoginModal={setIsOpenLoginModal} isOpenLoginModal={isOpenLoginModal} />
 
       <main
         className="flex-grow bg-cover bg-top bg-no-repeat"
         style={{ backgroundImage: "url('/bg-gradient.png')" }}
       >
-        <HeroSection />
+        <HeroSection setIsOpenLoginModal={setIsOpenLoginModal} isOpenLoginModal={isOpenLoginModal}/>
         <AdvantagesSection />
       </main>
 
