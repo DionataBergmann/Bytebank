6e4c6885caf04d04e5a395599b70e123a4c87aa5
DionataBergmann <dionata.podewils@outlook.com>
2025-07-21 22:20:08 -0300

    :construction: Migrate transaction logic to Redux Toolkit

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,32 +1,35 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '@/store'
+
 import MainLayout from '@/components/layout/MainLayout'
 import BalanceCard from '@/components/ui/BalanceCard'
 import NewTransaction from '@/components/ui/NewTransaction'
 import TransactionList from '@/components/ui/TransactionList'
-import { useTransactions } from '@/hooks/useTransactions'
+import { addTransaction, deleteTransaction, fetchTransactions, setEditingState, updateTransaction } from '@/store/transactionSlice'
 
 export default function HomePage() {
-  const {
-    transactions,
-    addTransaction,
-    handleDeleteTransaction,
-    handleEditTransaction,
-    handleSaveTransaction,
-  } = useTransactions()
+  const dispatch = useDispatch<AppDispatch>()
+  const { transactions, loading } = useSelector((state: RootState) => state.transactions)
+
+  useEffect(() => {
+    dispatch(fetchTransactions())
+  }, [dispatch])
 
   return (
     <MainLayout>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-40 mx-auto max-w-6xl">
         <div className="md:col-span-2 flex flex-col gap-6">
           <BalanceCard transactions={transactions} />
-          <NewTransaction addTransaction={addTransaction} />
+          <NewTransaction addTransaction={(tx) => dispatch(addTransaction(tx))} />
         </div>
 
         <div>
           <TransactionList
             transactions={transactions}
-            onDelete={handleDeleteTransaction}
-            onEdit={handleEditTransaction}
-            onSave={handleSaveTransaction}
+            onDelete={(id) => dispatch(deleteTransaction(id))}
+            onEdit={(id) => dispatch(setEditingState(id))}
+            onSave={(id, updated) => dispatch(updateTransaction({ id, updated }))}
           />
         </div>
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SnackbarProvider } from 'notistack'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SnackbarProvider
-      maxSnack={3}
-      autoHideDuration={3000}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      preventDuplicate
-    >
-      <Component {...pageProps} />
-    </SnackbarProvider>
+    <Provider store={store}>
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={3000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        preventDuplicate
+      >
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </Provider>
   )
 }
